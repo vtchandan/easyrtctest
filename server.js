759aaa7f6ce49e4e321934686a843a28be8f246b
vtchandan <vtchandan@gmail.com>
2014-05-12 22:58:25 +0530

    test

---
+++ server.js
@@ -0,0 +1,17 @@
+// Load required modules
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var io      = require("socket.io");         // web socket external module
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var httpApp = express();
+httpApp.configure(function() {
+    httpApp.use(express.static(__dirname + "/static/"));
+});
+
+// Start Express http server on port 8080
+var webServer = http.createServer(httpApp).listen(8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
