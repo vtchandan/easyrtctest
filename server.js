472ab86bf90c4169d14f1b29f9a5ac52448a7a5c
vtchandan <vtchandan@gmail.com>
2014-05-13 22:29:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var express = require("express");           // web framework external module
          // web socket external module
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
-var httpApp = express();
+var httpApp = express().createServer();
 httpApp.configure(function() {
     httpApp.use(express.static(__dirname + "/static/"));
 });
 
-// Start Express http server on port 8080
 var webServer = http.createServer(httpApp).listen(8080);
+
+
 
 
