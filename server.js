42f8aa78daa388a5d23ed2c026f6ab54b5d9715f
vtchandan <vtchandan@gmail.com>
2014-05-13 23:07:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Load required modules
 var http    = require("http");              // http server core module
-
+var port = process.env.PORT || 1337;
 
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(1337);
+}).listen(port);
 
 
 
