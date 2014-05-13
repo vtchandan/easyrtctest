d10011cedc4fd2cc9de158635df2b72cd31ad0c5
vtchandan <vtchandan@gmail.com>
2014-05-13 23:04:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Load required modules
 var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
-         // web socket external module
+
 
 
 var webServer = http.createServer(function(req, res) {
