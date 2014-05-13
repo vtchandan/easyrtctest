f44647793b8edec310d89f28504ab1bd30237883
vtchandan <vtchandan@gmail.com>
2014-05-13 22:59:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var http    = require("http");              // http server core module
 var express = require("express");           // web framework external module
          // web socket external module
 
-// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
-var httpApp = express().createServer();
-httpApp.configure(function() {
-    httpApp.use(express.static(__dirname + "/static/"));
-});
 
-var webServer = http.createServer(httpApp).listen(8080);
+var webServer = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(8080);
 
 
 
